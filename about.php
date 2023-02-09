3aaef0f5b462b034e8edda9dffd0f27fa5b89f12
nishanka <nishanka.rupasinghe@gmail.com>
2023-02-10 03:59:44 +1000

    Add About page content block styles

--- about.php
+++ about.php
@@ -5,46 +5,47 @@
         <?php include './main/header.php';?>
 
         <section class="main-content">
-            <div class="who-am-i">
+            <div class="content-block content-block--left box-shadow-green who-am-i" data-aos="fade-right">
                 <h3>Who am I?</h3>
                 <p>I love to code beautiful designs into user friendly Web Pages and Email Templates.</p>
             </div>
-            
-            <div class="skills">
+            <div class="content-block content-block--right box-shadow-green hard-skills" data-aos="fade-left">
                 <h3>Hard Skills</h3>
-
                 <ul>
                     <li>Develop and maintainance of websites and web application user interfaces.</li>
                     <li>Utilizing HTML, CSS and JavaScript to bring design ideas to life. </li>
-                    <li></li>
-                    <li>Building tools that enhance interaction with the site in any browser</li>
+                    <!-- <li>xxx</li> -->
+                    <!-- <li>Building tools that enhance interaction with the site in any browser</li>
                     <li>Implementing design for mobile sites</li>
                     <li>Maintaining software workflow management</li>
                     <li>Implementing best practice SEO</li>
                     <li>Testing the site for usability and fixing bugs and errors</li>
                     <li></li>
                     <li></li>
-                    <li></li>
-                </ul>
-
+                    <li></li> -->
+                </ul>                
+            </div>
+            <div class="content-block content-block--left box-shadow-green soft-skills" data-aos="fade-up-right">
                 <h3>Soft Skills</h3>
                 <ul>
                     <li>Listen to Listen</li>
                     <li>Time management</li>
                     <li>Adaptability</li>
                 </ul>
-                
+            </div>
+            <div class="content-block content-block--right box-shadow-green advance-skills" data-aos="fade-up-left">
                 <h3>Advance Skills</h3>
-
                 <ul>
                     <li>React</li>
+                    <!-- <li>xxxx</li>
                     <li>xxxx</li>
                     <li>xxxx</li>
-                    <li>xxxx</li>
-                    <li>XXX</li>
+                    <li>XXX</li> -->
                 </ul>
             </div>
-            <h3>Strengths</h3>
+            <div class="content-block content-block--left box-shadow-green strengths" data-aos="flip-up">
+                <h3>Strengths</h3>
+            </div>
         </section>
 
         <footer>
